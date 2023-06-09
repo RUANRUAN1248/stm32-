5ea3819d4de461fd7e90999ab2e0d83e6de806b7
RUANRUAN1248 <63839994+RUANRUAN1248@users.noreply.github.com>
2023-06-09 18:59:06 +0800

    ClockInit.c

--- system/ClockInit.c
+++ system/ClockInit.c
@@ -13,21 +13,3 @@ void SystemClock(uint16_t PLL_Pin){
 	while(!((RCC->CFGR & 0x03 << 2) == 0x08));
 
 }
-// void SystemInit(){
-// 	RCC->CR |= 0x01 << 16;	
-// 	uint16_t sign = 0;
-// 	while((RCC->CR & (0x01 << 17)) == 0){
-// 		if(sign == 1111){
-// 			break;
-// 		}
-// 		sign++;
-// 	}
-// 	FLASH->ACR |= 0x02;
-// 	RCC->CFGR |= (0x04 << 8);
-// 	RCC->CFGR |= (9 - 2) << 18; 
-// 	RCC->CFGR |= 0x01 << 16;
-// 	RCC->CR |= (0x01 << 24);
-// 	while((RCC->CFGR & (0x01 << 25)) == 0);
-// 	RCC->CFGR |= 0x02;
-// 	while(!((RCC->CFGR & 0x0c) == 0x08));
-// }
